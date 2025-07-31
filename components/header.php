7be272ce72fd4c36ca04d8b73cb76fac73afc94e
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-31 11:48:15 -0300

    separating into PHP components

---
+++ components/header.php
@@ -0,0 +1,12 @@
+<header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
+  <div class="font-bold text-xl text-cyan-600">🐧 Meu portfólio...</div>
+
+  <div>
+    <ul class="flex gap-x-3 font-medium text-gray-200">
+      <li><a href="#projetos" class="hover:underline">Projetos</a></li>
+      <li><a href="" class="hover:underline">GitHub</a></li>
+      <li><a href="" class="hover:underline">LinkedIn</a></li>
+      <li><a href="" class="hover:underline">Twitter</a></li>
+    </ul>
+  </div>
+</header>
