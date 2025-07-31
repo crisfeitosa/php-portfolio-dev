b465dec4c34b4fe5ee2a305c1ebb5ac208a573f8
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-31 11:55:55 -0300

    making the header dynamic

--- components/header.php
+++ components/header.php
@@ -1,12 +1,24 @@
+<?php 
+  $items = [
+    ['href' => '#projetos', 'text' => 'Projetos'],
+    ['href' => '#', 'text' => 'GitHub'],
+    ['href' => '#', 'text' => 'LinkedIn'],
+    ['href' => '#', 'text' => 'Twitter']
+  ];
+?>
+
 <header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
   <div class="font-bold text-xl text-cyan-600">🐧 Meu portfólio...</div>
 
   <div>
     <ul class="flex gap-x-3 font-medium text-gray-200">
-      <li><a href="#projetos" class="hover:underline">Projetos</a></li>
-      <li><a href="" class="hover:underline">GitHub</a></li>
-      <li><a href="" class="hover:underline">LinkedIn</a></li>
-      <li><a href="" class="hover:underline">Twitter</a></li>
+      <?php foreach($items as $item): ?>
+        <li>
+          <a href="<?= $item['href'] ?>" class="hover:underline">
+            <?= $item['text'] ?>
+          </a>
+        </li>
+      <?php endforeach; ?>
     </ul>
   </div>
 </header>
